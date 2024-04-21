8c138790d5c216446cf3f9dfa91b63adac3950e7
Georgie Jacob <56916479+Highbury14@users.noreply.github.com>
2024-04-21 18:34:19 +0530

    fix: comment unwanted sections

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,11 @@ import streamlit as st
 """
 # Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
+'''Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
+In the meantime, below is an example of what you can do with just a few lines of code:'''
 """
 
 number1 = st.number_input("Please input the first number.")
@@ -32,7 +32,7 @@ if (number3 > greatest1):
 
 st.write("The largest number is ", greatest1)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+'''num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
 indices = np.linspace(0, 1, num_points)
@@ -57,3 +57,4 @@ st.altair_chart(alt.Chart(df, height=700, width=700)
         color=alt.Color("idx", legend=None, scale=alt.Scale()),
         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
     ))
+'''
