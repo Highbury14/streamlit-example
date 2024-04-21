e5de4fb27affadad877e42f93ac9f06e1d2af018
Georgie Jacob <56916479+Highbury14@users.noreply.github.com>
2024-04-21 18:45:12 +0530

    fix: description-text

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,13 @@ import streamlit as st
 """
 # Welcome to Streamlit!
 
-[]: # "Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:"
+In the meantime, below is an example of what you can do with just a few lines of code:
+
+Find the largest of three numbers.
 """
 
 number1 = st.number_input("Please input the first number.")
