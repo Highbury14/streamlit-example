46c0ecf1afc137463117deb1d0c96ab9ec682f50
Georgie Jacob <56916479+Highbury14@users.noreply.github.com>
2024-04-22 10:46:10 +0530

    fix: remove unwanted text

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,9 @@ import streamlit as st
 """
 # Welcome to Streamlit! App by George J. , 21f3002389@ds.study.iitm.ac.in, Tools-for-DataScience.
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-
 Find the largest of three numbers.
 """
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 number1 = st.number_input("Please input the first number.")
 st.write("The first number is ", number1)
 
