1619ed08d22edc390e00cf01a7a14adefea08042
Georgie Jacob <56916479+Highbury14@users.noreply.github.com>
2024-04-22 10:52:19 +0530

    fix: screen-width configuration

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+def set_wide_page():
+  st.set_page_config(layout="wide")
+set_wide_page()
 
 """
 # Welcome to Streamlit! App by George J. , 21f3002389@ds.study.iitm.ac.in, Tools-for-DataScience.
 
 Find the largest of three numbers.
 """
-def set_wide_page():
-  st.set_page_config(layout="wide")
-set_wide_page()
 
 number1 = st.number_input("Please input the first number.")
 st.write("The first number is ", number1)
